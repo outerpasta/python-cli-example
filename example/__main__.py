4b4e4d8bd28e590667eb611c6d247126d030f403
alexanderbarry <outerpasta@gmail.com>
2017-02-21 13:52:51 -0800

    master: Setup cli test

--- example/__main__.py
+++ example/__main__.py
@@ -3,16 +3,26 @@
 """
 Example cli tool
 """
+import sys
+import os
 import argh
 
 from example.commands import COMMANDS
 
 def main():
-    """Main entry point for the cli."""
+    """
+    Main entry point for the cli.
+    """
     parser = argh.ArghParser()
     parser.add_commands(COMMANDS)
     parser.description = __doc__
     parser.dispatch()
+
+
+def test_cli():
+    """test"""
+    from subprocess import check_call
+    assert check_call([os.path.join(os.path.split(sys.executable)[0], 'example'), 'help']) == 0
 
 
 if __name__ == '__main__':
